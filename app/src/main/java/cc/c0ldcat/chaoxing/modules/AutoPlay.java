f68a29c0abc41886562de0afefbc4a2d7701ac23
ccat3z <c0ldcat3z@gmail.com>
2020-03-14 21:24:34 +0800

    fix AutoPlay for 4.3.6

--- app/src/main/java/cc/c0ldcat/chaoxing/modules/AutoPlay.java
+++ app/src/main/java/cc/c0ldcat/chaoxing/modules/AutoPlay.java
@@ -7,38 +7,18 @@ import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class AutoPlay implements IXposedHookLoadPackage {
-    private Class<?> cardWebViewClass;
-    private Class<?> loadingListenerInterfaceClass;
-    private Class<?> cardContentFragmentLoadingListenerClass;
-
-    private Map<Object, WebView> listenerToWebViewClassMap = new HashMap<>();
-
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) {
-        cardWebViewClass = XposedHelpers.findClassIfExists("com.chaoxing.fanya.aphone.view.CardWebView", loadPackageParam.classLoader);
-        loadingListenerInterfaceClass = XposedHelpers.findClassIfExists("com.chaoxing.fanya.aphone.view.CardWebView$b", loadPackageParam.classLoader);
-        cardContentFragmentLoadingListenerClass = XposedHelpers.findClassIfExists("com.chaoxing.fanya.aphone.ui.chapter.CardContentFragment$1", loadPackageParam.classLoader);
+        Class<?> webViewClientClass = XposedHelpers.findClass("android.webkit.WebViewClient", loadPackageParam.classLoader);
+        Class<?> webViewClass =XposedHelpers.findClass("android.webkit.WebView", loadPackageParam.classLoader);
 
-        XposedHelpers.findAndHookMethod(cardWebViewClass, "setLoadingListener", loadingListenerInterfaceClass, new XC_MethodHook() {
+        XposedHelpers.findAndHookMethod(webViewClientClass, "onPageFinished", webViewClass, String.class, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                listenerToWebViewClassMap.put(param.args[0], (WebView) param.thisObject);
-            }
-        });
+                LogUtils.i("try to click /ananas/css/play.png in 5s");
 
-        XposedHelpers.findAndHookMethod(cardContentFragmentLoadingListenerClass, "onload", boolean.class, new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                if (! (boolean) param.args[0]) {
-                    LogUtils.i("try to click video in 5s");
-
-                    // TODO: need a more stable JS
-                    listenerToWebViewClassMap.get(param.thisObject).loadUrl("javascript:" + "setTimeout(function (){$('iframe').contents().find('#ext-gen1040').click();}, 5000)");
-                }
+                ((WebView) param.args[0]).evaluateJavascript("javascript:" + "setTimeout(function (){$('iframe').contents().find('[src=\"/ananas/css/play.png\"]').click();}, 5000)", null);
             }
         });
     }
