adb223fe82e98c253c28c6ccdccf02aa7bded930
ccat3z <c0ldcat3z@gmail.com>
2020-03-17 16:23:17 +0800

    ViewerService: add Observable AccessibilityEvents

--- app/src/main/java/xyz/ccat3z/chaoxingviewer/ViewerService.kt
+++ app/src/main/java/xyz/ccat3z/chaoxingviewer/ViewerService.kt
@@ -3,13 +3,26 @@ package xyz.ccat3z.chaoxingviewer
 import android.accessibilityservice.AccessibilityService
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
+import io.reactivex.rxjava3.subjects.PublishSubject
 
 class ViewerService : AccessibilityService() {
+    val accessibilityEvents = PublishSubject.create<AccessibilityEvent>()!!
+
     override fun onServiceConnected() {
         Log.d(Common.LOG_TAG, "service started")
     }
 
     override fun onAccessibilityEvent(accessibilityEvent: AccessibilityEvent) {
+        val packageName = accessibilityEvent.packageName
+        if (packageName == null || packageName != "com.chaoxing.mobile") return
+        accessibilityEvents.onNext(accessibilityEvent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        accessibilityEvents.onComplete()
+
+        Log.d(Common.LOG_TAG, "service stopped")
     }
 
     override fun onInterrupt() {}
