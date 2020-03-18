bfc9a2899f07ca4b41395544e1d7d685c2cc6b0a
ccat3z <c0ldcat3z@gmail.com>
2020-03-18 16:09:11 +0800

    Common: add recordError

--- app/src/main/java/xyz/ccat3z/chaoxingviewer/Common.kt
+++ app/src/main/java/xyz/ccat3z/chaoxingviewer/Common.kt
@@ -1,5 +1,11 @@
 package xyz.ccat3z.chaoxingviewer
 
+import android.util.Log
+
 object Common {
     const val LOG_TAG = "xyz.ccat3z.cxviewer"
+
+    fun recordError(e: Throwable?) {
+        Log.e(LOG_TAG, e.toString());
+    }
 }
