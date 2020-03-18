d812e3c6c57aba6f8b51adfe1614799a00781610
ccat3z <c0ldcat3z@gmail.com>
2020-03-18 18:35:04 +0800

    ViewerService: log events

--- app/src/main/java/xyz/ccat3z/chaoxingviewer/ViewerService.kt
+++ app/src/main/java/xyz/ccat3z/chaoxingviewer/ViewerService.kt
@@ -15,6 +15,7 @@ class ViewerService : AccessibilityService() {
     override fun onAccessibilityEvent(accessibilityEvent: AccessibilityEvent) {
         val packageName = accessibilityEvent.packageName
         if (packageName == null || packageName != "com.chaoxing.mobile") return
+        Log.d("Viewer", "received event: $accessibilityEvent")
         accessibilityEvents.onNext(accessibilityEvent)
     }
 
