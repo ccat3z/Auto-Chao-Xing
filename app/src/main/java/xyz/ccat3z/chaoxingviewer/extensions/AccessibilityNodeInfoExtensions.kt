6d2aea32d1772007deb39d5485b399fd1fe7785e
ccat3z <c0ldcat3z@gmail.com>
2020-03-18 18:37:27 +0800

    add AccessibilityNodeInfo.findFirst

--- app/src/main/java/xyz/ccat3z/chaoxingviewer/extensions/AccessibilityNodeInfoExtensions.kt
+++ app/src/main/java/xyz/ccat3z/chaoxingviewer/extensions/AccessibilityNodeInfoExtensions.kt
@@ -4,3 +4,14 @@ import android.view.accessibility.AccessibilityNodeInfo
 
 fun AccessibilityNodeInfo.findSingleAccessibilityNodeInfoByViewId(viewId: String) =
         this.findAccessibilityNodeInfosByViewId(viewId)?.getOrNull(0)
+
+fun AccessibilityNodeInfo.findFirst(condition: (node: AccessibilityNodeInfo) -> Boolean): AccessibilityNodeInfo? {
+    if (condition(this)) return this
+
+    for (i in 0 until this.childCount) {
+        val res = this.getChild(i)?.findFirst(condition)
+        if (res != null) return res
+    }
+
+    return null
+}
